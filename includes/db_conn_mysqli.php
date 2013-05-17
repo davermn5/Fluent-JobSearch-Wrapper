61377123866066b93f799932f8ad21f37cdf2111
davermn5 <davermn5@gmail.com>
2013-05-16 18:59:53 -0700

    Update db_conn_mysqli.php
    Removed git comments

--- includes/db_conn_mysqli.php
+++ includes/db_conn_mysqli.php
@@ -4,11 +4,8 @@
   class dbConnectionMySQLi{
   
    public $_mysqli = null;
-<<<<<<< HEAD
    private $_token  = "";
-=======
    private $_token  = "jJ!1910jJ!1910";
->>>>>>> dd3ecf2d70d93a4844eb6c50e5677de75c205c3d
   
    public function __construct(){
     $mysqli = new mysqli('localhost', 'root', $this->_token, 'crawler');
