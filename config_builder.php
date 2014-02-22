18929ff29ddaa6a0d8bc612b4eccd086b5c63375
Dave Roman <davermn5@gmail.com>
2014-02-22 11:10:38 -0800

    Changed file and class name, and built functionality for returning multiple request_url strings which share the same ConfigBuilder instance.

---
+++ config_builder.php
@@ -0,0 +1,58 @@
+<?php
+
+include('bootstrap.php');
+
+class ConfigBuilder
+{
+	private $config = null;
+	private $request_url = "http://api.careerbuilder.com/V1/jobsearch?DeveloperKey=";
+	private $request_url_built = "";
+
+	public function __construct(Configuration $config){
+		$this->config = $config;
+		$this->initRequestUrl();
+	}
+
+	private function initRequestUrl(){
+		$this->request_url .= $this->config->cbapi->getKey();
+		$this->request_url_built = $this->request_url;
+	}
+
+	public function keyword($keyword){
+		$this->request_url_built .= "&Keywords={$keyword}";
+		return $this;
+	}
+
+	public function location($location){
+		$this->request_url_built .= "&Location={$location}";
+		return $this;
+	}
+
+	public function last($timeGroup, $defaultTimeGroup = 'month'){
+		$validGroups = array(
+			'day' => 1,
+			'swing' => 3,
+			'week' => 7,
+			'month' => 30
+		);
+
+		if(array_key_exists($timeGroup, $validGroups))
+		{
+			$defaultTimeGroup = $validGroups[$timeGroup];
+		}
+		else{
+			$defaultTimeGroup = $validGroups[$defaultTimeGroup];
+		}
+		$this->request_url_built .= "&PostedWithin={$defaultTimeGroup}";
+		return $this;
+	}
+
+	public function initialize(){
+		$currentState = $this->request_url_built;
+		$this->request_url_built = $this->request_url;
+		return $currentState;
+	}
+}
+
+$config_builder = new ConfigBuilder($config); //Defines how the built-up url string looks.
+echo $config_builder->last('day')->keyword('php')->location('denver')->initialize(); //Gives us the string.
