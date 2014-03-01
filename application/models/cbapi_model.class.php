afdc3d36a4ff3d7ebb497bf2a6da569ba09c23ca
Dave Roman <davermn5@gmail.com>
2014-03-01 15:15:47 -0800

    Re-formatted code.

--- application/models/cbapi_model.class.php
+++ application/models/cbapi_model.class.php
@@ -1,27 +1,27 @@
 <?php
 
-require_once('application/interfaces/helper_signature.interface.php');
+	require_once('application/interfaces/helper_signature.interface.php');
 
-class CbapiModel implements IHelperSignature
-{
-	public function getResponse($url, $returnType = ''){
-		$responseXmlObj = simplexml_load_file($url);
-		if( ($responseXmlObj instanceof SimpleXMLElement) !== true )
+	class CbapiModel implements IHelperSignature
+	{
+		public function getResponse($url, $returnType = '')
 		{
-			throw new UnexpectedValueException('The response did not return a SimpleXMLElement object.');
-		}
-		else{
-			$returnType = strtolower($returnType);
-			switch($returnType){
-				case 'array' :
-					$parser_model = new ParserModel();
-					return $parser_model->parseSimpleXmlObjectToArray($responseXmlObj);
-				break;
+			$responseXmlObj = simplexml_load_file($url);
+			if(($responseXmlObj instanceof SimpleXMLElement) !== true){
+				throw new UnexpectedValueException('The response did not return a SimpleXMLElement object.');
+			}else{
+				$returnType = strtolower($returnType);
+				switch ($returnType) {
+					case 'array' :
+						$parser_model = new ParserModel();
 
-				default :
-					return $responseXmlObj;
-				break;
+						return $parser_model->parseSimpleXmlObjectToArray($responseXmlObj);
+						break;
+
+					default :
+						return $responseXmlObj;
+						break;
+				}
 			}
 		}
 	}
-}
