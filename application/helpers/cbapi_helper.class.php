a0f05220bf94700a3f8b271dd5162165ae61700f
Dave Roman <davermn5@gmail.com>
2014-03-01 13:30:25 -0800

    Re-formatted code.

--- application/helpers/cbapi_helper.class.php
+++ application/helpers/cbapi_helper.class.php
@@ -1,26 +1,29 @@
 <?php
 
-require_once('application/interfaces/helper_signature.interface.php');
+	require_once('application/interfaces/helper_signature.interface.php');
 
-class CbapiHelper implements IHelperSignature
-{
-	public $model = null;
-	public $request_url;
+	class CbapiHelper implements IHelperSignature
+	{
+		public $model = null;
+		public $request_url;
 
-	public function __construct( IHelperSignature $model ){
-		$this->model = $model;
-	}
+		public function __construct(IHelperSignature $model)
+		{
+			$this->model = $model;
+		}
 
-	public function getResponse($request_url, $returnType){
-		try{
-			return $this->model->getResponse($request_url, $returnType);
-		}catch(UnexpectedValueException $e){
-			$this->showErrorMessage($e);
+		public function getResponse($request_url, $returnType)
+		{
+			try {
+				return $this->model->getResponse($request_url, $returnType);
+			} catch (UnexpectedValueException $e) {
+				$this->showErrorMessage($e);
+			}
+		}
+
+		private function showErrorMessage($e)
+		{
+			echo 'A ' . get_class($e) . ' type of exception was thrown with the following message: ' . $e->getMessage() . '</br></br>StackTrace:</br>'
+				. $e->getTraceAsString();
 		}
 	}
-
-	private function showErrorMessage($e){
-		echo 'A ' . get_class($e) . ' type of exception was thrown with the following message: ' . $e->getMessage() . '</br></br>StackTrace:</br>'
-			. $e->getTraceAsString();
-	}
-}
