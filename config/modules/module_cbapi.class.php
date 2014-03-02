3c89312492166b8304201092cd694f59c7ca4427
Dave Roman <davermn5@gmail.com>
2014-03-02 07:54:35 -0800

    Re-formatted code.

--- config/modules/module_cbapi.class.php
+++ config/modules/module_cbapi.class.php
@@ -8,57 +8,58 @@
 		private $_locationCity = false;
 		private $_relocationCovered = false;
 
-		public function __construct(array $cbapi_options)
-		{
+		public function __construct(array $cbapi_options) {
 			$this->validateUsableOptions($cbapi_options);
 		}
 
-		public function validateUsableOptions(array $cbapi_options)
-		{
-			foreach ($cbapi_options as $option => $val) {
-				if ($option == '_key') {
-					if (is_string($val)) {
+		public function validateUsableOptions(array $cbapi_options) {
+			foreach($cbapi_options as $option => $val)
+			{
+				if($option == '_key')
+				{
+					if(is_string($val))
+					{
 						$token = preg_replace('/\s+/', '', $val);
-						if (!empty($token)) {
+						if(!empty($token))
+						{
 							$this->_key = $token;
-						} else {
+						}else{
 							throw new UnexpectedValueException('The _key option must be a non-empty string!');
 						}
-					} else {
+					}else{
 						throw new UnexpectedValueException('The _key option must be a valid string!');
 					}
-				} else {
-					if ($option == '_blankApplicationServiceURL' || $option == '_locationCity' || $option == '_relocationCovered') {
-						if (filter_var($val, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE) == null) {
+				}else{
+					if($option == '_blankApplicationServiceURL' || $option == '_locationCity' || $option == '_relocationCovered')
+					{
+						if(filter_var($val, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE) == null)
+						{
 							throw new InvalidArgumentException("The {$option} option is not a valid boolean filter!");
-						} else {
+						}else{
 							$this->$option = $val;
 						}
-					} else {
+					}else{
 						throw new OutOfBoundsException("{$option} is not a valid option for the Cbapi API. ");
 					}
 				}
 			}
+
 			return $this;
 		}
 
-		public function getKey()
-		{
+		public function getKey() {
 			return $this->_key;
 		}
 
-		public function getBlankApplicationServiceURL()
-		{
+		public function getBlankApplicationServiceURL() {
 			return $this->_blankApplicationServiceURL;
 		}
 
-		public function getLocationCity()
-		{
+		public function getLocationCity() {
 			return $this->_locationCity;
 		}
 
-		public function getRelocationCovered()
-		{
+		public function getRelocationCovered() {
 			return $this->_relocationCovered;
 		}
 	}
