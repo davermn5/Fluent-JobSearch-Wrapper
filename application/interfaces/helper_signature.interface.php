59bedec6273fef65da44455f5384e7df844ce812
Dave Roman <davermn5@gmail.com>
2014-03-02 08:39:42 -0800

    Created documentation.

--- application/interfaces/helper_signature.interface.php
+++ application/interfaces/helper_signature.interface.php
@@ -1,5 +1,9 @@
 <?php
 
+	/**
+	 * This interface is used to create an abstract dependency between the
+	 *  cbapi helper and cbapi model classes.
+	 */
 	interface IHelperSignature
 	{
 		public function getResponse($request_url, $returnType);
