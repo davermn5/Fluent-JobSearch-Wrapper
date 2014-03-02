9d6e25ae367023d94c636fd365885dcfebe5f677
Dave Roman <davermn5@gmail.com>
2014-03-02 07:52:53 -0800

    Re-formatted code.

--- config/modules/module_mysqli.class.php
+++ config/modules/module_mysqli.class.php
@@ -13,8 +13,7 @@
 			'db_socket'
 		);
 
-		public function __construct(array $mysqli_options)
-		{
+		public function __construct(array $mysqli_options) {
 			$this->validateUsableOptions($mysqli_options);
 
 			$mysqli = new mysqli($this->parsedCredentials['db_server'],
@@ -23,24 +22,27 @@
 				$this->parsedCredentials['db_database'],
 				$this->parsedCredentials['db_port'],
 				$this->parsedCredentials['db_socket']);
-			if (mysqli_connect_error()) {
+			if(mysqli_connect_error())
+			{
 				throw new mysqli_sql_exception('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
-			} else {
+			}else{
 				return $mysqli;
 			}
 		}
 
-		public function validateUsableOptions(array $mysqli_options)
-		{
-			foreach ($mysqli_options as $option => $val) {
-				if (in_array($option, $this->usable_options)) {
-					if (is_string($val)) {
-						$token = preg_replace('/\s+/', '', $val);
+		public function validateUsableOptions(array $mysqli_options) {
+			foreach($mysqli_options as $option => $val)
+			{
+				if(in_array($option, $this->usable_options))
+				{
+					if(is_string($val))
+					{
+						$token                            = preg_replace('/\s+/', '', $val);
 						$this->parsedCredentials[$option] = $token;
-					} else {
+					}else{
 						throw new UnexpectedValueException("The {$option} option must be a valid string!");
 					}
-				} else {
+				}else{
 					throw new OutOfBoundsException("{$option} is a non-supported option for Mysqli. ");
 				}
 			}
