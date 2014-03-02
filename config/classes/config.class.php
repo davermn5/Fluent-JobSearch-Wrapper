b956605d7cf59b999f16afab84eb5c17aaed1bb6
Dave Roman <davermn5@gmail.com>
2014-03-02 07:59:01 -0800

    Re-formatted code.

--- config/classes/config.class.php
+++ config/classes/config.class.php
@@ -8,21 +8,22 @@
 		public $mysqli = null;
 		public $cbapi = null;
 
-		public function __construct(array $module_options)
-		{
+		public function __construct(array $module_options) {
 			$this->mysqli = array_key_exists('mysqli_options', $module_options) ? new MySQLiModule($module_options['mysqli_options']) : null;
-			$this->cbapi = array_key_exists('cbapi_options', $module_options) ? new CbapiModule($module_options['cbapi_options']) : null;
+			$this->cbapi  = array_key_exists('cbapi_options', $module_options) ? new CbapiModule($module_options['cbapi_options']) : null;
 
 			$this->initConfig();
 		}
 
-		private function initConfig()
-		{
-			if ($this->mysqli == null) {
+		private function initConfig() {
+			if($this->mysqli == null)
+			{
 				throw new DomainException("The required mysqli_options module could not be found inside its parent container module_options. Please re-check the bootstrap file.");
-			} elseif ($this->cbapi == null) {
+			}
+			elseif($this->cbapi == null)
+			{
 				throw new DomainException("The required cbapi_options module could not be found inside its parent container module_options. Please re-check the bootstrap file.");
-			} else {
+			}else{
 				return $this;
 			}
 		}
