3dcaa8001d4331d4226a6f7b176135da217c7f3b
Dave Roman <davermn5@gmail.com>
2014-03-01 16:00:26 -0800

    Created documentation.

--- application/models/business_logic/parser_model.class.php
+++ application/models/business_logic/parser_model.class.php
@@ -1,7 +1,19 @@
 <?php
 
+	/**
+	 * The purpose of this class is to provide a set of low-level business logic for
+	 *  parsing data returned from the model class (protocol).
+	 */
 	class ParserModel
 	{
+		/**
+		 * Parses a SimpleXMLElement object into an array
+		 *
+		 * @param SimpleXMLElement (object) $xml The SimpleXMLElement object to parse
+		 *
+		 * @return Returns an array upon success
+		 * @throws UnexpectedValueException
+		 */
 		public function parseSimpleXmlObjectToArray($xml)
 		{
 			$traversable = json_decode(json_encode($xml), true);
