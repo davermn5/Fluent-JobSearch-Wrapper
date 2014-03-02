7ddebdfd07834d6cfe0eacf1c95756cdf17bd5a2
Dave Roman <davermn5@gmail.com>
2014-03-02 07:49:07 -0800

    Re-formatted code.

--- application/models/business_logic/parser_model.class.php
+++ application/models/business_logic/parser_model.class.php
@@ -14,10 +14,10 @@
 		 * @return Returns an array upon success
 		 * @throws UnexpectedValueException
 		 */
-		public function parseSimpleXmlObjectToArray($xml)
-		{
+		public function parseSimpleXmlObjectToArray($xml) {
 			$traversable = json_decode(json_encode($xml), true);
-			if(!is_array($traversable)){
+			if(!is_array($traversable))
+			{
 				throw new UnexpectedValueException('The conversion from object to array was un-successful.');
 			}else{
 				return $traversable;
