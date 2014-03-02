de9bf81df7bc59609fae0284badb2856acc88597
Dave Roman <davermn5@gmail.com>
2014-03-02 08:02:01 -0800

    Re-formatted code.

--- bootstrap.php
+++ bootstrap.php
@@ -5,27 +5,27 @@
 	$module_options = array(
 
 		'mysqli_options' => array(
-			'db_server' => "127.0.0.1",
-			'db_user' => "",
+			'db_server'   => "127.0.0.1",
+			'db_user'     => "",
 			'db_password' => "",
 			'db_database' => "cbapi",
-			'db_port' => "3306",
-			'db_socket' => ""
+			'db_port'     => "3306",
+			'db_socket'   => ""
 		),
 
-		'cbapi_options' => array(
-			'_key' => 'WDTZ14P67NZKL453DBTN',
+		'cbapi_options'  => array(
+			'_key'                        => 'WDTZ14P67NZKL453DBTN',
 			'_blankApplicationServiceURL' => true,
-			'_locationCity' => true,
-			'_relocationCovered' => true
+			'_locationCity'               => true,
+			'_relocationCovered'          => true
 		)
 
 	);
 
 	global $config;
-	try{
+	try {
 		$config = new Config($module_options);
 		require_once('config/builder.class.php');
-	}catch(Exception $e){
+	} catch (Exception $e) {
 		echo 'Bootstrap: A ' . get_class($e) . ' type of exception was thrown with the following message: ' . $e->getMessage();
 	}
